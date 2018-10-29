9df6ca94fe862d5628c8bede114f6b04351a6dd4
Anastasia Mironova <anast.mir22@yandex.ru>
2018-10-29 19:53:09 +0300

    Render field

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+     int [] [] test = {{2,1,2},{1,2,1},{0,0,1}};
+
+     RenderField kek =  new RenderField();
+     kek.checkSymbol(test);
     }
 }
